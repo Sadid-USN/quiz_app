33650badfdc390c0a89a80f9936fd6873abef906
MYNAME <myname@MacBook-Pro-MYNAME.local>
2022-09-03 22:11:16 +0600

    added quiz screen

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -8,6 +8,7 @@ class QuizScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('Quiz Screen'),
         centerTitle: true,
       ),
